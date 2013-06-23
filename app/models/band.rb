5f09c2025cf92472288ca41b1db8bfff0f14a8d7
rares123 <cruceat.rares@yahoo.com>
2013-06-23 22:46:08 +0300

    Add the RoR app

---
+++ app/models/band.rb
@@ -0,0 +1,3 @@
+class Band < ActiveRecord::Base
+  attr_accessible :desription, :name
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppForBands::Application.config.secret_token = 'aacf7817563c299b78c9d4e25d074da94fa9a7a373a5c296154c24b68312f33620660c068408c37e4e83b915029569022f8f3eab87c92b07e98023d44ac070cb'
